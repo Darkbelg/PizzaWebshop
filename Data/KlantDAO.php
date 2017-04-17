5c54e5c18dea9627e46f401687262a3269f35193
Darkbelg <stijn.sagaert@outlook.com>
2017-04-17 19:11:05 +0200

    met registreren account

--- Data/KlantDAO.php
+++ Data/KlantDAO.php
@@ -14,97 +14,145 @@ require_once "Entities/Stad.php";
 require_once "Entities/Klant.php";
 require_once "Data/StraatDAO.php";
 require_once "Data/StadDAO.php";
-require_once "Exception/BestaatException";
+require_once "Exceptions/BestaatException.php";
 
 
 class KlantDAO
 {
-	public function getKlanten()
-	{
-		$dbh=DBConfig::openConnectie();
-		$sql = "SELECT klantNummer,naam,voornaam,telefoon, emailadres as email,wachtwoord,opmerking,promo,beheerder,
+    public function getKlanten()
+    {
+        $dbh = DBConfig::openConnectie();
+        $sql = "SELECT klantNummer,naam,voornaam,telefoon, emailadres as email,wachtwoord,opmerking,promo,beheerder,
 stad.id as stadId,postcode,stad,
 straat.id as straatId,straat,huisnummer 
 FROM klant INNER JOIN stad on klant.stadId = stad.id 
 INNER JOIN straat on straat.id = klant.straatId";
-		$resultSet = $dbh->query($sql);
-		$lijst = array();
-		foreach ($resultSet as $rij){
-			$straat = Straat::create($rij["straatId"],$rij["straat"],$rij["huisnummer"]);
-			$stad = Stad::create($rij["stadId"],$rij["postcode"],$rij["stad"]);
-			$klant = Klant::create($rij["klantNummer"],$rij["naam"],$rij["voornaam"],$rij["telefoon"],$rij["email"],$rij["wachtwoord"],$rij["opmerking"],$rij["promo"],$rij["beheerder"],$stad,$straat);
-			array_push($lijst,$klant);
-		}
-		$dbh=DBConfig::sluitConnectie();
-		return $lijst;
+        $resultSet = $dbh->query($sql);
+        $lijst = array();
+        foreach ($resultSet as $rij) {
+            $straat = Straat::create($rij["straatId"], $rij["straat"], $rij["huisnummer"]);
+            $stad = Stad::create($rij["stadId"], $rij["postcode"], $rij["stad"]);
+            $klant = Klant::create($rij["klantNummer"], $rij["naam"], $rij["voornaam"], $rij["telefoon"], $rij["email"], $rij["wachtwoord"], $rij["opmerking"], $rij["promo"], $rij["beheerder"], $stad, $straat);
+            array_push($lijst, $klant);
+        }
+        $dbh = DBConfig::sluitConnectie();
+        return $lijst;
 
-	}
+    }
 
-	public function getAccountByEmail($email)
-	{
-		$dbh = DBConfig::openConnectie();
-		$sql = "SELECT klantNummer,naam,voornaam,telefoon, emailadres as email,wachtwoord,opmerking,promo,beheerder,
+    public function getAccountByEmail($email)
+    {
+        $dbh = DBConfig::openConnectie();
+        $sql = "SELECT klantNummer,naam,voornaam,telefoon, emailadres as email,wachtwoord,opmerking,promo,beheerder,
 stad.id as stadId,postcode,stad,
 straat.id as straatId,straat,huisnummer 
 FROM klant INNER JOIN stad on klant.stadId = stad.id 
 INNER JOIN straat on straat.id = klant.straatId
 where emailadres = :email";
-		$stmt = $dbh->prepare($sql);
-		$stmt->execute(array(":email"=>$email));
-		$rij = $stmt->fetch(PDO::FETCH_ASSOC);
-		$straat = Straat::create($rij["straatId"],$rij["straat"],$rij["huisnummer"]);
-		$stad = Stad::create($rij["stadId"],$rij["postcode"],$rij["stad"]);
-		$klant = Klant::create($rij["klantNummer"],$rij["naam"],$rij["voornaam"],$rij["telefoon"],$rij["email"],$rij["wachtwoord"],$rij["opmerking"],$rij["promo"],$rij["beheerder"],$stad,$straat);
-		return $klant;
-	}
-	public function getAccountById($klantNummer)
-	{
-		$dbh = DBConfig::openConnectie();
-		$sql = "SELECT klantNummer,naam,voornaam,telefoon, emailadres as email,wachtwoord,opmerking,promo,beheerder,
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array(":email" => $email));
+        $rij = $stmt->fetch(PDO::FETCH_ASSOC);
+        $straat = Straat::create($rij["straatId"], $rij["straat"], $rij["huisnummer"]);
+        $stad = Stad::create($rij["stadId"], $rij["postcode"], $rij["stad"]);
+        $klant = Klant::create($rij["klantNummer"], $rij["naam"], $rij["voornaam"], $rij["telefoon"], $rij["email"], $rij["wachtwoord"], $rij["opmerking"], $rij["promo"], $rij["beheerder"], $stad, $straat);
+        return $klant;
+    }
+
+    public function getAccountById($klantNummer)
+    {
+        $dbh = DBConfig::openConnectie();
+        $sql = "SELECT klantNummer,naam,voornaam,telefoon, emailadres as email,wachtwoord,opmerking,promo,beheerder,
 stad.id as stadId,postcode,stad,
 straat.id as straatId,straat,huisnummer 
 FROM klant INNER JOIN stad on klant.stadId = stad.id 
 INNER JOIN straat on straat.id = klant.straatId
 where klantNummer = :klantNummer";
-		$stmt = $dbh->prepare($sql);
-		$stmt->execute(array(":klantNummer"=>$klantNummer));
-		$rij = $stmt->fetch(PDO::FETCH_ASSOC);
-		$straat = Straat::create($rij["straatId"],$rij["straat"],$rij["huisnummer"]);
-		$stad = Stad::create($rij["stadId"],$rij["postcode"],$rij["stad"]);
-		$klant = Klant::create($rij["klantNummer"],$rij["naam"],$rij["voornaam"],$rij["telefoon"],$rij["email"],$rij["wachtwoord"],$rij["opmerking"],$rij["promo"],$rij["beheerder"],$stad,$straat);
-		return $klant;
-	}
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array(":klantNummer" => $klantNummer));
+        $rij = $stmt->fetch(PDO::FETCH_ASSOC);
+        $straat = Straat::create($rij["straatId"], $rij["straat"], $rij["huisnummer"]);
+        $stad = Stad::create($rij["stadId"], $rij["postcode"], $rij["stad"]);
+        $klant = Klant::create($rij["klantNummer"], $rij["naam"], $rij["voornaam"], $rij["telefoon"], $rij["email"], $rij["wachtwoord"], $rij["opmerking"], $rij["promo"], $rij["beheerder"], $stad, $straat);
+        return $klant;
+    }
 
-	public function create($naam,$voornaam,$telefoon,$straat,$huisnummer,$stad,$postcode,$email="",$wachtwoord="")
-	{
 
-		//TODO aanmaken van de klant
+    /**
+     * Alle gegevens voor een nieuwe klant maar geen email of wachtwoord
+     * @param $naam
+     * @param $voornaam
+     * @param $telefoon
+     * @param $straat
+     * @param $huisnummer
+     * @param $stad
+     * @param $postcode
+     * @return klant
+     */
 
-		$stadDao = new StadDAO();
-		$straatDao=new StraatDAO();
+    public function create($naam, $voornaam, $telefoon, $straat, $huisnummer, $stad, $postcode)
+    {
+        $bestaatKlant = $this->bestaatKlant($naam, $voornaam, $telefoon);
 
-		try {
-			$stad = $stadDao->create($stad,$postcode);
-		}
-		catch (BestaatException $ex){
-			$stad = $stadDao->getByStad($stad);
-		}
-		try {
-			$straat = $straatDao->create($straat, $huisnummer);
-		}
-		catch (BestaatException $ex){
-			$straat = $straatDao->getByStraat($straat, $huisnummer);
-		}
-		$dbh = DBConfig::openConnectie();
-		$sql = "insert into klant naam=:naam,voornaam=:voornaam,telefoon=:telefoon,emailadres=:email,wachtwoord=:wachtwoord,stadId=:stadId,straatIs=:straatId";
-		$stmt = $dbh->prepare($sql);
-		$stmt->execute(array(":naam"=>$naam,":voornaam"=>$voornaam,":telefoon"=>$telefoon,":email"=>$email,":wachtwoord"=>$wachtwoord,":stadId"=>$stad->getId(),":straatId"=>$straat->getId()));
-		$id = $dbh->lastInsertId();
-		$dbh = DBConfig::sluitConnectie();
-		$klant = $this->getAccountById($id);
-		return $id;
+        if ($bestaatKlant) {
 
-	}
+            return $klant = $this->getAccountById($bestaatKlant);
+        }
+
+        $stadDao = new StadDAO();
+        $straatDao = new StraatDAO();
+
+        try {
+            $stad = $stadDao->create($stad, $postcode);
+        } catch (BestaatException $ex) {
+            $stad = $stadDao->getByStad($stad);
+        }
+        try {
+            $straat = $straatDao->create($straat, $huisnummer);
+        } catch (StraatBestaatException $ex) {
+            $straat = $straatDao->getByStraat($straat, $huisnummer);
+        }
+
+        $dbh = DBConfig::openConnectie();
+        $sql = "insert into klant(naam,voornaam,telefoon,stadId,straatId) VALUES (:naam,:voornaam,:telefoon,:stadId,:straatId)";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array(":naam" => $naam, ":voornaam" => $voornaam, ":telefoon" => $telefoon, ":stadId" => $stad->getId(), ":straatId" => $straat->getId()));
+
+        $id = $dbh->lastInsertId();
+        $dbh = DBConfig::sluitConnectie();
+        $klant = $this->getAccountById($id);
+
+        return $klant;
+    }
+
+    /**
+     * @param $naam
+     * @param $voornaam
+     * @param $telefoon
+     */
+    private function bestaatKlant($naam, $voornaam, $telefoon)
+    {
+        $dbh = DBConfig::openConnectie();
+        $sql = "select klantNummer from klant where naam=:naam and voornaam=:voornaam and telefoon=:telefoon";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array(":naam" => $naam, ":voornaam" => $voornaam, ":telefoon" => $telefoon));
+        $rij = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $rij["klantNummer"];
+    }
+
+    public function registreerAccount($email, $wachtwoord, $klantNummer)
+    {
+        $wachtHash = password_hash($wachtwoord, PASSWORD_DEFAULT);
+        $dbh = DBConfig::openConnectie();
+        $sql = "update klant set emailadres=:email,wachtwoord=:wachtwoord WHERE klantNummer=:klantNummer ";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array(":email" => $email, ":wachtwoord" => $wachtHash, ":klantNummer" => $klantNummer));
+
+        $dbh = DBConfig::sluitConnectie();
+        $klant = $this->getAccountById($klantNummer);
+        return $klant;
+    }
+
 
 
 }
