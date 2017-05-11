5152bdf9bb51bbebb58b7f3b7b010fda48b2f7f2
Darkbelg <stijn.sagaert@outlook.com>
2017-05-11 10:50:06 +0200

    met bootstrap en min naming bug

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,24 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit707bef3af5e21227235c8f565f428f83
 {
+    public static $files = array (
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+    );
+
+    public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+    );
+
     public static $prefixesPsr0 = array (
         'T' => 
         array (
@@ -19,6 +37,8 @@ class ComposerStaticInit707bef3af5e21227235c8f565f428f83
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit707bef3af5e21227235c8f565f428f83::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit707bef3af5e21227235c8f565f428f83::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit707bef3af5e21227235c8f565f428f83::$prefixesPsr0;
 
         }, null, ClassLoader::class);
