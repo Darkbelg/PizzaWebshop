69698a25021c1ec10156c7782d7a6db3a0efa1d4
Darkbelg <stijn.sagaert@outlook.com>
2017-05-11 10:22:52 +0200

    update connection info voor op heroku te connecteren met online database

--- Data/DBConfig.php
+++ Data/DBConfig.php
@@ -10,9 +10,9 @@
 
 class DBConfig
 {
-	public static $DB_CONNSTRING = "mysql:host=localhost;dbname=pizza;charset=utf8";
-	public static $DB_USERNAME = "Papi";
-	public static $DB_PASSWORD = "Pizza";
+	public static $DB_CONNSTRING = "mysql:host=eu-mm-auto-dub-01-b.cleardb.net;dbname=heroku_6a983ac56ffdd25;charset=utf8";
+	public static $DB_USERNAME = "bc1116b511b6d8";
+	public static $DB_PASSWORD = "c7796c42";
 
 	public static function openConnectie(){
 		return new PDO(DBConfig::$DB_CONNSTRING,DBConfig::$DB_USERNAME,DBConfig::$DB_PASSWORD);
