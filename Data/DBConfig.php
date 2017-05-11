b44cbdec2e1f58d03150d99be8f8e58aa3fce081
Darkbelg <stijn.sagaert@outlook.com>
2017-05-11 11:41:22 +0200

    Updated database config with gloabal variable

--- Data/DBConfig.php
+++ Data/DBConfig.php
@@ -10,12 +10,18 @@
 
 class DBConfig
 {
-	public static $DB_CONNSTRING = "mysql:host=eu-mm-auto-dub-01-b.cleardb.net;dbname=heroku_6a983ac56ffdd25;charset=utf8";
-	public static $DB_USERNAME = "bc1116b511b6d8";
-	public static $DB_PASSWORD = "c7796c42";
+
+//	public static $DB_CONNSTRING = "mysql:host=eu-mm-auto-dub-01-b.cleardb.net;dbname=heroku_6a983ac56ffdd25;charset=utf8";
+//	public static $DB_USERNAME = "bc1116b511b6d8";
+//	public static $DB_PASSWORD = "c7796c42";
+
 
 	public static function openConnectie(){
-		return new PDO(DBConfig::$DB_CONNSTRING,DBConfig::$DB_USERNAME,DBConfig::$DB_PASSWORD);
+		$DB_CONNSTRING = $_ENV["DB_CONNSTRING"];
+		$DB_USERNAME = $_ENV["DB_USERNAME"];
+		$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+
+		return new PDO($DB_CONNSTRING,$DB_USERNAME,$DB_PASSWORD);
 	}
 
 	public static function sluitConnectie()
