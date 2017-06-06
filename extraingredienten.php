91709e6adbf900724e9eb756b97c0db61c709f42
Darkbelg <stijn.sagaert@outlook.com>
2017-06-06 11:32:59 +0200

    met correcte link naar allepizzas en lowercase presentation

--- extraingredienten.php
+++ extraingredienten.php
@@ -8,12 +8,10 @@
 	require_once("bootstrap.php");
 	require_once("Business/ProductService.php");
 	require_once("login.php");
-
 	$productServ = new ProductService();
 	$product = $productServ->getById($_GET["id"]);
 	$twigarray["pizza"] = $product;
 	$extras = $productServ->getAllByToday();
 	$twigarray["extras"] = $extras;
-
 	$view = $twig->render("extraingredienten.twig",$twigarray);
 	print ($view);
